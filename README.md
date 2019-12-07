a55d552673829ebd75ee09bf3651c6614d23c019
lovac42 <lovac42@protonmail.com>
2019-12-06 19:40:22 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Doublethink
-AnkiAddon: UTF-8 Friendly Characters
+# Doublethink: Allows UTF-8 Friendly Characters
+
+## About:
+Prevent normalization of contents during imports, edits, and browser searches.
---
+++ src/doublethink/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright: (C) 2019 Lovac42
+# Support: https://github.com/lovac42/Doublethink
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+
+import sys
+import unicodedata
+
+
+normalized=unicodedata.normalize
+
+def unnormalized_passthrough_filter(form, txt):
+    if form=="NFC":
+        for i in range (1,5): #filter calling function
+            try:
+                if sys._getframe(i).f_code.co_name in (
+                    "onBridgeCmd", "importNotes", "_onSearchActivated"
+                ):
+                    return txt
+            except ValueError:
+                break
+    return normalized(form,txt)
+
+unicodedata.normalize=unnormalized_passthrough_filter
