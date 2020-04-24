88b5967a4bc09e8afe9d3f601c650b21bfcd365e
lovac42 <lovac42@protonmail.com>
2020-04-23 23:01:30 -0400

    bump version to v1.0.0

--- src/build.bat
+++ src/build.bat
@@ -3,7 +3,7 @@ set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=doublethink
 set NAME=Doublethink
 set PACKID=1654528592
-set VERSION=0.0.2
+set VERSION=1.0.0
 
 
 echo %VERSION% >%REPO%\VERSION
