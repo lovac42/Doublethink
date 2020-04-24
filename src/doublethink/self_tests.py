2d88871e5a03caa48fa0b1e2739da9d6054ac406
lovac42 <lovac42@protonmail.com>
2020-04-23 22:55:46 -0400

    Enhanced tests to check for method names

--- src/doublethink/self_tests.py
+++ src/doublethink/self_tests.py
@@ -4,28 +4,47 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 
-# This only tests for correct wrapping.
-# It does not account for name changes.
-
 import unicodedata
 from anki.hooks import addHook
 
 
+
 def onBridgeCmd():
-    testWrap("欄廊朗虜")
+    "Called by card editor"
+    from aqt.editor import Editor
+    testMethodName(Editor, "onBridgeCmd")
+    testWrap("欄廊朗虜神")
+
 
 def importNotes():
-    testWrap("類猪神")
+    "Called by note importer"
+    from anki.importing.csvfile import TextImporter
+    testMethodName(TextImporter, "importNotes")
+    testWrap("朗虜類猪神")
+
 
 def _onSearchActivated():
-    testWrap("祥福諸")
+    "Called by browser searches"
+    from aqt.browser import Browser
+    testMethodName(Browser, "_onSearchActivated")
+    testWrap("欄廊祥福諸")
+
+
+
 
 def testWrap(non_norm):
+    "test if wrap was successful"
     processed = unicodedata.normalize("NFC", non_norm)
     assert non_norm == processed, "Addon Doublethink failed to work"
 
+
+def testMethodName(className, methodName):
+    "test if method name is still the same between different anki versions"
+    assert hasattr(className, methodName) and callable(getattr(className, methodName)), f"The method {methodName} required by the addon Doublethink is no longer available."
+
+
 def runTests():
-    for func in (onBridgeCmd,importNotes,_onSearchActivated):
+    for func in (onBridgeCmd, importNotes, _onSearchActivated):
         func()
     # print("Addon Doublethink loaded successfully")
 
