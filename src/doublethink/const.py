c6803a651cd6b0876d156d543ab4983d7a49a8ad
lovac42 <lovac42@protonmail.com>
2020-04-23 23:00:07 -0400

    Added version warning

--- src/doublethink/const.py
+++ src/doublethink/const.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright: (C) 2019-2020 Lovac42
+# Copyright: (C) 2020 Lovac42
 # Support: https://github.com/lovac42/Doublethink
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 
-from .lib.com.lovac42.anki.others import safety_first
+import os
+ADDON_PATH = os.path.dirname(__file__)
 
-from . import main
-from . import self_tests
+ADDON_NAME = "Doublethink"
+
+TARGET_STABLE_VERSION = 23
